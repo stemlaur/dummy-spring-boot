1d8d1b2d04c5d461d671a96fce19d6cf61f981fa
lstemmer <laurent-externe.stemmer@dalkia.fr>
2024-10-03 10:36:53 +0200

    ajout de cache pour résoudre
        // instances differentes de CommentRepository
        // reconstruction des beans à chaque appel à getBean

--- src/main/java/fr/dalkia/dummyspringboot/SpringContext.java
+++ src/main/java/fr/dalkia/dummyspringboot/SpringContext.java
@@ -2,15 +2,24 @@ package fr.dalkia.dummyspringboot;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class SpringContext {
 
+    private static final Map<Class, Object> CACHE = new HashMap<Class, Object>();
+
     public static <T> T getBean(Class<T> clazz) {
+        if (CACHE.containsKey(clazz)) {
+            return (T) CACHE.get(clazz);
+        }
         try {
             // Get constructor with no arguments
             Constructor<T> constructor = clazz.getDeclaredConstructor();
             constructor.setAccessible(true);
-            return constructor.newInstance();
+            T result = constructor.newInstance();
+            CACHE.put(clazz, result);
+            return result;
         } catch (NoSuchMethodException e) {
             // Try finding a constructor with arguments
             Constructor constructor = findConstructorWithArguments(clazz);
@@ -19,14 +28,16 @@ public class SpringContext {
                     // Simulate dependency injection (replace with actual implementation)
                     Object[] args = getConstructorArguments(constructor);
                     constructor.setAccessible(true);
-                    return (T) constructor.newInstance(args);
+                    Object result = constructor.newInstance(args);
+                    CACHE.put(clazz, result);
+                    return (T) result;
                 } catch (Exception ex) {
                     throw new RuntimeException("Error creating bean: " + clazz.getName(), ex);
                 }
             } else {
                 throw new RuntimeException("No suitable constructor found for: " + clazz.getName());
             }
-        } catch(InstantiationException | IllegalAccessException e){
+        } catch (InstantiationException | IllegalAccessException e) {
             throw new RuntimeException("Error creating bean: " + clazz.getName(), e);
         } catch (InvocationTargetException e) {
             throw new RuntimeException(e);
